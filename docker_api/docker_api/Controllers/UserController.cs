b77cc3a3e641f4000c27f86489542b00c3885018
mystoryme <my.storygeng@gmail.com>
2018-11-17 15:00:36 +0700

    Docker_api
    Docker_api

---
+++ docker_api/docker_api/Controllers/UserController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using docker_api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace docker_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly UserContext _context;
+        public UserController(UserContext context)
+        {
+            _context = context;
+
+            if (_context.Users.Count() == 0)
+            {
+                // Create a new TodoItem if collection is empty,
+                // which means you can't delete all TodoItems.
+                _context.Users.Add(new User { Name = "Item1" });
+                _context.SaveChanges();
+
+            }
+
+
+        }
+
+        [HttpGet]
+        public ActionResult<List<User>> GetAll()
+        {
+            return _context.Users.ToList();
+        }
+
+
+
+        [HttpGet("{id}", Name = "GetUser")]
+        public ActionResult<User> GetById(long id)
+        {
+            var item = _context.Users.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return item;
+        }
+
+
+        [HttpPost]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(201)]
+        public IActionResult Create(User item)
+        {
+            if (item.Name == null || item.Username == null || item.Password == null || item.Email == null || item.Age == 0)
+            {
+
+                return StatusCode(400);
+                //return BadRequest();
+                //return NoContent();
+                //Responses
+            }
+            else
+            {
+                _context.Users.Add(item);
+                _context.SaveChanges();
+
+                return CreatedAtRoute("GetUser", new { id = item.Id }, item);
+            }
+
+        }
+
+
+        [HttpPut("{id}")]
+        public IActionResult Update(long id, User item)
+        {
+            var todo = _context.Users.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            todo.Name = item.Name;
+            todo.Username = item.Username;
+            todo.Password = item.Password;
+            todo.Age = item.Age;
+
+            _context.Users.Update(todo);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var todo = _context.Users.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(todo);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
---
+++ docker_api/docker_api/Models/UserContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace docker_api.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasIndex(item => new { item.Email })
+                .IsUnique(true);
+        }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
